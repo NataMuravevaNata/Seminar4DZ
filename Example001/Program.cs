2f736a89fb6f2e5939530f7229fbd739534b9c2e
Nata <muravevantsh@mail.ru>
2022-08-18 16:21:36 +0300

    Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+Console.WriteLine("Введите k1");
+int k1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите b1");
+int b1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите k2");
+int k2 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите b2");
+int b2 = int.Parse(Console.ReadLine() ?? "0");
+
+if ((k1 == k2) && (b1 == b2))
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+Console.WriteLine("Точка пересечения x:");
+Console.WriteLine((b2-b1)/(k1-k2));
+Console.WriteLine("Точка пересечения y:");
+Console.WriteLine((k1*(b2-b1))/(k1-k2)+b1);
+}
